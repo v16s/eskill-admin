0b2e05b10d1d4549b145b481c2ba83a80e4fd248
v16s <shankar.vignesh@gmail.com>
2019-04-08 13:02:25 +0530

    WIP report

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import LoadScreen from './LoadScreen'
+import React, { Component } from 'react';
+import axios from 'axios';
+import LoadScreen from './LoadScreen';
+import { Row, Col } from 'antd';
 
 export default class Report extends Component {
-  state = {}
-  componentDidMount () {
-    let { tid } = this.props
+  state = {};
+  componentDidMount() {
+    let { tid } = this.props;
     axios.get('http://localhost:3000/api/faculty/reports/' + tid).then(res => {
       if (res.data.success) {
         Promise.all(
@@ -18,55 +19,73 @@ export default class Report extends Component {
                       .post('http://localhost:3000/api/faculty/answer', {
                         n: q.n,
                         course: r.course,
-                        branch: r.branch
+                        branch: r.branch,
                       })
                       .then(result => {
                         resolve({
                           correct: result.data.a,
                           your: q.answer,
-                          status: result.data.a == q.answer
-                        })
-                      })
-                  })
+                          status: result.data.a == q.answer,
+                        });
+                      });
+                  });
                 })
               ).then(res => {
-                resolve({ result: res, username: r.username })
-              })
-            })
+                let score = 0,
+                  max = res.length;
+                res.map(a => {
+                  if (a.status) {
+                    score++;
+                  }
+                });
+                resolve({ result: res, username: r.username, score, max });
+              });
+            });
           })
         ).then(res => {
-          this.setState({ report: res })
-        })
+          this.setState({ report: res });
+        });
       }
-    })
+    });
   }
 
-  render () {
-    let { report } = this.state
+  render() {
+    let { report } = this.state;
+    let shape = {
+      height: '40px',
+      width: '40px',
+      border: '1px solid #fff',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      color: '#fff',
+    };
     if (report) {
       return (
         <div>
           {report.map(r => (
             <div>
-              {r.username}
-              {r.result.map(a => (
-                <div>
-                  {a.status ? (
-                    <p>RIGHT!</p>
-                  ) : (
-                    <div>
-                      <p>WRONG!</p>
-                      <p>Student's answer: {a.your}</p>
-                      <p>Corret answer: {a.correct}</p>
-                    </div>
-                  )}
-                </div>
-              ))}
+              {r.username}, {r.score}/{r.max}
+              <div style={{ display: 'flex' }}>
+                {r.result.map(a => (
+                  <div
+                    style={
+                      a.status
+                        ? { ...shape, background: '#00ff00' }
+                        : { ...shape, background: '#ff0000' }
+                    }
+                  >
+                    {!a.status && a.your == ''
+                      ? 'N/A'
+                      : `${a.your}: ${a.correct}`}
+                  </div>
+                ))}
+              </div>
             </div>
           ))}
         </div>
-      )
+      );
     }
-    return <LoadScreen />
+    return <LoadScreen />;
   }
 }
